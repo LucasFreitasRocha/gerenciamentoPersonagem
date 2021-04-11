7d3aa7b2333bf87248b2a3f8996b762c333d4099
Lucas Rocha <rochadefreitaslucas@gmail.com>
2021-04-11 13:59:46 -0300

    iniciando thymeleaf

---
+++ src/main/java/com/gp/controllers/view/UsuarioControllerView.java
@@ -0,0 +1,10 @@
+package com.gp.controllers.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/users")
+public abstract class UsuarioControllerView {
+	
+}
